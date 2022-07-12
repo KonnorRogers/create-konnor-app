38e2a324de3be62aa07fba37790cf41a9899a8cc
ParamagicDev <konnor5456@gmail.com>
2022-07-12 10:52:09 -0400

    fixing some cli stuff

--- src/index.ts
+++ src/index.ts
@@ -1,121 +1,138 @@
 #!/usr/bin/env node
-import * as fs from 'fs';
-import * as path from 'path';
-import inquirer from 'inquirer';
-import chalk from 'chalk';
-import shell from 'shelljs';
-import * as ejs from 'ejs';
-import * as url from 'url';
+import * as fs from "fs";
+import * as path from "path";
+import inquirer from "inquirer";
+import chalk from "chalk";
+import shell from "shelljs";
+import * as ejs from "ejs";
+import * as url from "url";
 
 interface TemplateData {
-  projectName: string
+  projectName: string;
+  typescript: boolean;
 }
 
 interface CliOptions {
-  projectName: string
-  templateName: string
-  templatePath: string
-  targetPath: string
+  projectName: string;
+  templateName: string;
+  templatePath: string;
+  targetPath: string;
+  typescript: boolean;
 }
-
 
 const __filename = path.join(process.cwd(), url.fileURLToPath(import.meta.url));
 const __dirname = path.dirname(__filename);
 
-const SKIP_FILES = ['node_modules', '.template.json'];
-
+const SKIP_FILES = ["pnpm-lock.yaml", "node_modules", ".template.json"];
 
 function render(content: string, data: TemplateData) {
   return ejs.render(content, data);
 }
 
-console.log(path.resolve(__dirname, 'templates'))
-const CHOICES = fs.readdirSync(path.resolve(__dirname, 'templates'));
+const CHOICES = fs.readdirSync(path.resolve(__dirname, "templates"));
 const QUESTIONS = [
-{
-    name: 'template',
-    type: 'list',
-    message: 'What template would you like to use?',
-    choices: CHOICES
-},
-{
-    name: 'name',
-    type: 'input',
-    message: 'Please input a new project name:'
-}];
+  {
+    name: "template",
+    type: "list",
+    message: "What template would you like to use?",
+    choices: CHOICES,
+  },
+  {
+    name: "typescript",
+    type: "confirm",
+    message: "Would you like to use Typescript? (Y/N)",
+  },
+  {
+    name: "name",
+    type: "input",
+    message: "Please input a new project name:",
+  },
+];
 
 const CURR_DIR = process.cwd();
 
-inquirer.prompt(QUESTIONS).then(answers => {
-    const projectChoice = answers['template'];
-    const projectName = answers['name'];
-    const templatePath = path.join(__dirname, 'templates', projectChoice);
-    const targetPath = path.join(CURR_DIR, projectName);
+inquirer.prompt(QUESTIONS).then((answers) => {
+  const projectChoice = answers["template"];
+  const projectName = answers["name"];
+  const typescript = answers["typescript"];
+  const templatePath = path.join(__dirname, "templates", projectChoice);
+  const targetPath = path.join(CURR_DIR, projectName);
 
-    const options: CliOptions = {
-      projectName,
-      templateName: projectChoice,
-      templatePath,
-      targetPath
-    }
+  const options: CliOptions = {
+    projectName,
+    templateName: projectChoice,
+    templatePath,
+    targetPath,
+    typescript,
+  };
 
-    if (!createProject(targetPath)) {
-      return;
-    }
+  if (!createProject(targetPath)) {
+    return;
+  }
 
-    createDirectoryContents(templatePath, projectName);
+  createDirectoryContents(templatePath, projectName);
 
-    postProcess(options);
+  postProcess(options);
 });
 
 function createProject(projectPath: string) {
-    if (fs.existsSync(projectPath)) {
-        console.log(chalk.red(`Folder ${projectPath} exists. Delete or use another name.`));
-        return false;
-    }
-    fs.mkdirSync(projectPath);
+  if (fs.existsSync(projectPath)) {
+    console.log(
+      chalk.red(`Folder ${projectPath} exists. Delete or use another name.`)
+    );
+    return false;
+  }
+  fs.mkdirSync(projectPath);
 
-    return true;
+  return true;
 }
 
+function createDirectoryContents(
+  templatePath: string,
+  { projectName, typescript }: TemplateData
+) {
+  // read all files/folders (1 level) from template folder
+  const filesToCreate = fs.readdirSync(templatePath);
+  // loop each file/folder
+  filesToCreate.forEach((file) => {
+    const origFilePath = path.join(templatePath, file);
 
-function createDirectoryContents(templatePath: string, projectName: string) {
-    // read all files/folders (1 level) from template folder
-    const filesToCreate = fs.readdirSync(templatePath);
-    // loop each file/folder
-    filesToCreate.forEach(file => {
-        const origFilePath = path.join(templatePath, file);
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
 
-        // get stats about the current file
-        const stats = fs.statSync(origFilePath);
+    // skip files that should not be copied
+    if (SKIP_FILES.indexOf(file) > -1) return;
 
-        // skip files that should not be copied
-        if (SKIP_FILES.indexOf(file) > -1) return;
-
-        if (stats.isFile()) {
-            // read file content and transform it using template engine
-            let contents = fs.readFileSync(origFilePath, 'utf8');
-            contents = render(contents, { projectName });
-            // write file to destination folder
-            const writePath = path.join(CURR_DIR, projectName, file);
-            fs.writeFileSync(writePath, contents, 'utf8');
-        } else if (stats.isDirectory()) {
-            // create folder in destination folder
-            fs.mkdirSync(path.join(CURR_DIR, projectName, file));
-            // copy files/folder inside current folder recursively
-            createDirectoryContents(path.join(templatePath, file), path.join(projectName, file));
+    if (stats.isFile()) {
+      // read file content and transform it using template engine
+      let contents = fs.readFileSync(origFilePath, "utf8");
+      contents = render(contents, { projectName, typescript });
+      // write file to destination folder
+      const writePath = path.join(CURR_DIR, projectName, file);
+      fs.writeFileSync(writePath, contents, "utf8");
+    } else if (stats.isDirectory()) {
+      // create folder in destination folder
+      fs.mkdirSync(path.join(CURR_DIR, projectName, file));
+      // copy files/folder inside current folder recursively
+      createDirectoryContents(
+        path.join(templatePath, file),
+        {
+          projectName: path.join(projectName, file),
+          typescript
         }
-    });
+      );
+    }
+  });
 }
 
 function postProcess(options: CliOptions) {
-  const isNode = fs.existsSync(path.join(options.templatePath, 'package.json'));
+  const isNode = fs.existsSync(path.join(options.templatePath, "package.json"));
   if (isNode) {
-      shell.cd(options.targetPath);
-      const result = shell.exec('pnpm install');
-      if (result.code !== 0) {
-          return false;
-      }
+    shell.cd(options.targetPath);
+    const result = shell.exec("pnpm install");
+    if (result.code !== 0) {
+      return false;
+    }
   }
 
   return true;
