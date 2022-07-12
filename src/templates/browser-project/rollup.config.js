8024e6c5f55fe94e5f7dc61583de609ac740b9f9
ParamagicDev <konnor5456@gmail.com>
2022-07-12 10:39:37 -0400

    inital commit

---
+++ src/templates/browser-project/rollup.config.js
@@ -0,0 +1,74 @@
+import * as path from "path";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import { brotliCompressSync } from "zlib";
+import gzipPlugin from "rollup-plugin-gzip";
+import glob from "glob"
+
+let entries = {}
+glob
+  .sync("src/**/*.{ts,js}")
+  .forEach((file) => {
+    const key = path.join(path.dirname(file), path.basename(file, path.extname(file)))
+    entries[key] = file
+  });
+
+export default [
+  {
+    input: "src/index.js",
+    plugins: [compressionPlugins()],
+    output: [
+      {
+        file: "dist/bundle/<%= projectName %>.umd.js",
+        format: "umd",
+        name: "<%= projectName %>",
+        esModule: false,
+      },
+      {
+        file: "dist/bundle/<%= projectName %>.module.js",
+        format: "es",
+      }
+    ]
+  },
+  {
+    input: entries,
+    plugins: basePlugins(),
+    output: [
+      {
+        dir: "dist",
+        format: "es",
+      },
+    ],
+  },
+];
+
+function basePlugins(tsconfig = "./tsconfig.json") {
+  return [
+    resolve(),
+
+    <% if (typescript) { %>
+      typescript({ tsconfig })
+    <% else { %>
+      // typescript({ tsconfig })
+    <% } %>
+  ];
+}
+
+function compressionPlugins(tsconfig = "./tsconfig.json") {
+  return [
+    ...basePlugins(tsconfig),
+    terser({
+      compress: {
+        passes: 10,
+      },
+    }),
+    // GZIP compression as .gz files
+    gzipPlugin(),
+    // Brotil compression as .br files
+    gzipPlugin({
+      customCompression: (content) => brotliCompressSync(Buffer.from(content)),
+      fileName: ".br",
+    }),
+  ];
+}
